c7ea9e935302286379a561fb01ad63a980bfe07f
Jean-Louis GUENEGO <jlguenego@gmail.com>
2026-04-21 15:59:05 +0200

    feat: added eslint

--- back/server.js
+++ back/server.js
@@ -5,11 +5,12 @@ console.log("Starting ' server...");
 const app = express();
 
 app.get("/", (req, res) => {
+    if (req === res) {
+        throw new Error("oups");
+    }
     res.send("Hello World");
 });
 
 app.listen(3000, () => {
     console.log("Server is running on http://localhost:3000");
 });
-
-console.log("rtrtrt")[1].map((x) => x + 1);

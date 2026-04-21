1bd72f892897407f71da597f05e1c69f6b185b2e
Jean-Louis GUENEGO <jlguenego@gmail.com>
2026-04-21 15:13:48 +0200

    feat: add prettierrc

--- back/server.js
+++ back/server.js
@@ -1,14 +1,15 @@
 import express from "express";
-console.log('express: ', express);
 
+console.log("Starting ' server...");
 
-console.log("Starting server...");
+const app = express();
 
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
 
+app.listen(3000, () => {
+    console.log("Server is running on http://localhost:3000");
+});
 
-
-
-
-
-
-
+console.log("rtrtrt")[1].map((x) => x + 1);

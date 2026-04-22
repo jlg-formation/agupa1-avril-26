25fbeb18a35c6eeafa05df366efa74d53716959d
Jean-Louis GUENEGO <jlguenego@gmail.com>
2026-04-22 11:31:46 +0200

    feat: added routing step 2/4

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Legal } from './pages/legal/legal';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'legal', component: Legal },
+];

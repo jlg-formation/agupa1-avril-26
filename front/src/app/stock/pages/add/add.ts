9a2d48ad312e436dffb4ac58eb405d38ae113b89
Jean-Louis GUENEGO <jlguenego@gmail.com>
2026-04-22 17:18:33 +0200

    feat: add @for

--- front/src/app/stock/pages/add/add.ts
+++ front/src/app/stock/pages/add/add.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-add',
-  imports: [],
+  imports: [FaIconComponent],
   templateUrl: './add.html',
   styleUrl: './add.scss',
 })
-export class Add {}
+export class Add {
+  faPlus = faPlus;
+}
--- front/src/app/stock/pages/list/list.ts
+++ front/src/app/stock/pages/list/list.ts
@@ -1,7 +1,14 @@
+import { CommonModule, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 import { faRotateRight, faPlus, faTrashCan } from '@fortawesome/free-solid-svg-icons';
+
+const articles = [
+  { id: 'a1', name: 'Tournevis', price: 10.99, qty: 456 },
+  { id: 'a2', name: 'Pelle', price: 4.99, qty: 12 },
+  { id: 'a3', name: 'Marteau', price: 8.99, qty: 23 },
+];
 
 @Component({
   selector: 'app-list',
@@ -13,4 +20,6 @@ export class List {
   faRotateRight = faRotateRight;
   faPlus = faPlus;
   faTrashCan = faTrashCan;
+
+  articles = articles;
 }

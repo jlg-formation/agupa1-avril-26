95acc0506e2efa12fe50b1a7bf78033ddb22c6b4
Jean-Louis GUENEGO <jlguenego@gmail.com>
2026-04-22 16:32:33 +0200

    feat: add liste page

--- front/src/app/stock/pages/list/list.ts
+++ front/src/app/stock/pages/list/list.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { faRotateRight, faPlus, faTrashCan } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-list',
-  imports: [RouterLink],
+  imports: [RouterLink, FaIconComponent],
   templateUrl: './list.html',
   styleUrl: './list.scss',
 })
-export class List {}
+export class List {
+  faRotateRight = faRotateRight;
+  faPlus = faPlus;
+  faTrashCan = faTrashCan;
+}

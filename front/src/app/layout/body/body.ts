de0ec7ce406f320b47c2a92b342f84cbffe5d7f5
Jean-Louis GUENEGO <jlguenego@gmail.com>
2026-04-22 11:47:30 +0200

    feat: added routing 3/4

--- front/src/app/layout/body/body.ts
+++ front/src/app/layout/body/body.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-body',
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './body.html',
   styleUrl: './body.scss',
 })

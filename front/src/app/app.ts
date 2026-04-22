787a81914335cc5e04f6534aeeea699d4ff2e3b2
Jean-Louis GUENEGO <jlguenego@gmail.com>
2026-04-22 10:43:22 +0200

    feat: added header

--- front/src/app/app.ts
+++ front/src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Body } from './layout/body/body';
+import { Footer } from './layout/footer/footer';
+import { Header } from './layout/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Header, Body, Footer],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('front');
